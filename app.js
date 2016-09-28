7fa773b6261fe0bb96b047867dc15bf451d76caa
Matthew Capewell <matthewcapewell@googlemail.com>
2016-09-28 10:48:51 +0100

    Added Seb to list of Gurnard players

--- app.js
+++ app.js
@@ -34,6 +34,9 @@ var match =
                 "name": "Luca"
             },
             {
+                "name": "Sebastian"
+            },
+            {
                 "name": "Sef"
             },
             {
